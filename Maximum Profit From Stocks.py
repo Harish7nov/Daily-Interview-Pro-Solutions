47392db7b601d2c2adbdfc58dddf88421675b93a
Harish7nov <48373859+Harish7nov@users.noreply.github.com>
2019-11-19 12:56:12 +0530

    Update Maximum Profit From Stocks.py

--- Maximum Profit From Stocks.py
+++ Maximum Profit From Stocks.py
@@ -1,52 +1,9 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 7,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "def buy_and_sell(arr):\n",
-    "    return(arr[-1]-min(arr))"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 8,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "9\n"
-     ]
-    }
-   ],
-   "source": [
-    "print(buy_and_sell([9, 1, 11, 8, 5, 7, 10]))"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.3"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+''' You are given an array. Each element represents the price of a stock on that particular day. Calculate and return the maximum
+	profit you can make from buying and selling that stock only once. For example: [9, 11, 8, 5, 7, 10]. Here, the optimal trade is to
+	buy when the price is 5, and sell when it is 10, so the return value should be 5 (profit = 10 - 5 = 5).'''
+
+def buy_and_sell(arr):
+    return(arr[-1]-min(arr))
+
+print(buy_and_sell([9, 11, 8, 5, 7, 10]))
+# Output - 5
